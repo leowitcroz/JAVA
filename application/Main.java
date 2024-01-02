35dadfe268e66c1db206e4f566511b5e4421e26b
Leonardo <leowitcroz@gmail.com>
2024-01-02 16:55:05 -0300

    mesmo problema agora com OOP

--- application/Main.java
+++ application/Main.java
@@ -3,32 +3,34 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Main {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+
+        Triangle x, y;
+
+        x = new Triangle();
+        y = new Triangle();
 
         System.out.println("Enter de measures of the triangle X: ");
 
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b  = sc.nextDouble();
+        x.c  = sc.nextDouble();
 
         System.out.println("Enter de measures of the triangle Y: ");
 
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double p = (xA + xB + xC) / 2;
+        double areaX = x.area();
 
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-        p = (yA + yB + yC) / 2;
-
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        double areaY = y.area();
 
         System.out.printf("Triangle X Area : %.4f%n", areaX);
         System.out.printf("Triangle Y Area : %.4f%n", areaY);
