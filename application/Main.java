ec3bbc488025d227f243c6da9f4fd127fbccec57
Leonardo <leowitcroz@gmail.com>
2024-01-05 12:33:37 -0300

    melhorando o problema de stock com constructor

--- application/Main.java
+++ application/Main.java
@@ -3,25 +3,43 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import utils.CurrencyConverter;
+import entities.Product;
 
 public class Main {
     public static void main(String[] args) {
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("What is the price of the dollar: ");
+        System.out.println("Enter Product data: ");
+        System.out.println("Name: ");
+        String name = sc.nextLine();
 
-        double dollar = sc.nextDouble();
+        System.out.println("Price: ");
+        double price = sc.nextDouble();
 
-        System.out.println("How many dollars will you buy: ");
-   
-        double quantityDollar = sc.nextDouble();
+        System.out.println("Quantity");
+        int quantity = sc.nextInt();
 
-        double priceToPay = CurrencyConverter.dollarToRealWithIof(dollar,quantityDollar);
+        Product product = new Product(name, price, quantity);
 
-        System.out.printf("Amount to be paid: %.2f%n", priceToPay);
+        System.err.println();
+        System.err.println("Product Data " + product.toString());
+
+        System.out.println("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.err.println("Updated Product Data " + product.toString());
+
+        System.err.println();
+
+        System.out.println("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.err.println("Updated Product Data " + product.toString());
 
         sc.close();
-    };
-}
+    }
+};
--- utils/CurrencyConverter.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class CurrencyConverter {
-    public static double dollarToRealWithIof(double dollar, double quantityDollar) {
-        return (dollar * quantityDollar) * 1.06;
-    }
-}
