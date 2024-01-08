1450f3bcd70a344fc39794bfe33d0079a23bfb23
Leonardo <leowitcroz@gmail.com>
2024-01-08 11:09:49 -0300

    encapsulamento

--- entities/Product.java
+++ entities/Product.java
@@ -1,11 +1,11 @@
 package entities;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
 
-    
+    private String name;
+    private double price;
+    private int quantity;
+
     public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
@@ -15,6 +15,22 @@ public class Product {
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
     }
 
     public double totalValueInStock() {
