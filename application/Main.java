eb2bd3cb0a5042ab2eed1d9935dde486d77ec77c
Leonardo <leowitcroz@gmail.com>
2024-01-11 10:43:15 -0300

    introduccao a vetores

--- application/Main.java
+++ application/Main.java
@@ -5,11 +5,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        int p;
+        int n = sc.nextInt();
 
-        p = 10;
+        double[] vect = new double[n];
 
-        System.err.println(p);
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            sum += vect[i];
+        }
+
+        double avg = sum / 3;
+
+        System.out.println(avg);
+
+        sc.close();
     }
 };
