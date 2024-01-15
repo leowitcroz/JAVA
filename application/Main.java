df2626b91c1937b2b6dc94075cc3e1b10d546b3b
Leonardo <leowitcroz@gmail.com>
2024-01-15 10:26:45 -0300

    for each in java

--- application/Main.java
+++ application/Main.java
@@ -3,38 +3,12 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Product;
-
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        String[] vect = new String[] { "Maria", "Bob", "Alex" };
 
-        int n = sc.nextInt();
-
-        Product[] vect = new Product[n];
-
-        for (int i = 0; i < vect.length; i++) {
-            sc.nextLine();
-            String name = sc.nextLine();
-
-            double price = sc.nextDouble();
-
-            vect[i] = new Product(name, price);
-
+        for(String nome : vect){
+            System.out.println(nome);
         }
-
-        double sum = 0.0;
-
-        for (int i = 0; i < vect.length; i++) {
-            sum += vect[i].getPrice();
-            ;
-        }
-
-        double avg = sum / vect.length;
-
-        System.out.printf("Avarege price: %.2f%n", avg);
-
-        sc.close();
     }
 };
